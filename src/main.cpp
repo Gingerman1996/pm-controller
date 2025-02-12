52f60257e6bb0a409707cef42f0a4b6e4dc84f41
Gingerman1996 <guide@airgradient.com>
2025-02-12 08:38:43 +0700

    Use lower levels

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@
 
 // Pin for monitoring loop
 int Trig1 = 2;
-#define _SR 4  // speed range
-#define NP 2   // number of TACH pulse per revolution
+#define _SR 4 // speed range
+#define NP 2  // number of TACH pulse per revolution
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -57,7 +57,7 @@ ESPNowMaster espNowMaster(ssid, password, APSSID);
 // NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 0,
-                     60000);  // Update time every 60 seconds
+                     60000); // Update time every 60 seconds
 
 // API URL
 const char *apiUrl =
@@ -65,19 +65,19 @@ const char *apiUrl =
     "current?token=dadf4720-44da-4906-bd4f-9a57d6e073a4";
 
 unsigned long bootTime =
-    millis();  // gives time since startup in ms. Overflow after 50 days.
+    millis(); // gives time since startup in ms. Overflow after 50 days.
 unsigned long t = millis();
 
 // Loop variables
-const int D = 50;              // delay between loops in ms
-unsigned long intervalTs = 0;  // timestamp for controlling interval
-unsigned long durationTs = 0;  // timestamp for controlling fan duration
-unsigned long pre2 = 0;        // timestamp for controlling print interval
-unsigned long pre2_1 = 0;      // timestamp for printing board data
-unsigned long pre2_11 = 0;     // timestamp for printing weighted average
-unsigned long pmsReadTs = 0;   // timestamp for controlling PMS reading interval
+const int D = 50;             // delay between loops in ms
+unsigned long intervalTs = 0; // timestamp for controlling interval
+unsigned long durationTs = 0; // timestamp for controlling fan duration
+unsigned long pre2 = 0;       // timestamp for controlling print interval
+unsigned long pre2_1 = 0;     // timestamp for printing board data
+unsigned long pre2_11 = 0;    // timestamp for printing weighted average
+unsigned long pmsReadTs = 0;  // timestamp for controlling PMS reading interval
 unsigned long printTimestamp =
-    0;  // timestamp for controlling print interval (3 seconds)
+    0; // timestamp for controlling print interval (3 seconds)
 unsigned int fanSpeedPercent = 0;
 float duration = 0;
 int InletConcentration = 0;
@@ -105,19 +105,19 @@ PMS local_pms(Serial1);
 const byte MAX31790 = 0x23;
 byte data, data0, data1;
 
-#define INIT_INTERVAL_SECONDS \
-  150  // initial interval between 1st pulse and next pulse (longer for
-       // distribution of aerosol)
-#define REG_INTERVAL_SECONDS \
-  90  // regulation interval between each pulse after the 1st to maintain pm2.5
-      // concentration
+#define INIT_INTERVAL_SECONDS                                                  \
+  150 // initial interval between 1st pulse and next pulse (longer for
+      // distribution of aerosol)
+#define REG_INTERVAL_SECONDS                                                   \
+  90 // regulation interval between each pulse after the 1st to maintain pm2.5
+     // concentration
 #define PMS_READ_INTERVAL_SECONDS 1
 unsigned int TARGET_PM02 =
-    0;  // target pm2.5 concentration in micrograms per cubic meter
+    0; // target pm2.5 concentration in micrograms per cubic meter
 
 const int targetValues[] = {5, 10, 20, 40, 100, 150};
 const float targetValuesWorldStandard[] = {0, 5, 9, 25, 35.4};
-const int maxHours = sizeof(targetValues) / sizeof(targetValues[0]);
+int maxHours;
 unsigned long currentNtpTime = 0;
 
 unsigned long lastUpdateTime = 0;
@@ -139,7 +139,7 @@ byte get_I2C_register(byte ADDRESS, byte REGISTER) {
   Wire.beginTransmission(ADDRESS);
   Wire.write(REGISTER);
   Wire.endTransmission();
-  Wire.requestFrom(ADDRESS, 1);  // read 1 byte
+  Wire.requestFrom(ADDRESS, 1); // read 1 byte
   byte x = Wire.read();
   return x;
 }
@@ -158,7 +158,7 @@ float calculateWeightedAverage(float values[], float weights[],
 
   // Return the weighted average
   if (totalWeight == 0) {
-    return 0;  // Prevent division by zero
+    return 0; // Prevent division by zero
   } else {
     return weightedSum / totalWeight;
   }
@@ -214,16 +214,16 @@ void setup() {
   // pAdvertising->start();
 
   // Reset MAX31790 (POR)
-  set_I2C_register(MAX31790, 0x00, B01010000);  // reset
+  set_I2C_register(MAX31790, 0x00, B01010000); // reset
   delay(100);
 
   // PWM Frequency       first 4 bits 0111 (0-10V), next 4 bits 1011 (PWM)
-  set_I2C_register(MAX31790, 0x01, B01101010);  // 1.25kHz
+  set_I2C_register(MAX31790, 0x01, B01101010); // 1.25kHz
   delay(1);
 
   // Configuration
-  data = B00001000;  // PWM mode - 'Spin-up' OFF - control ON - Tach count- PWM
-                     // yeah
+  data = B00001000; // PWM mode - 'Spin-up' OFF - control ON - Tach count- PWM
+                    // yeah
   for (int i = 0x02; i <= 0x07; i++) {
     set_I2C_register(MAX31790, i, data);
     delay(1);
@@ -296,6 +296,9 @@ void loop() {
       TARGET_PM02 = 5;
       currentHour = 1;
       lastUpdateTime = currentNtpTime;
+      // maxHours = sizeof(targetValues) / sizeof(targetValues[0]);
+      maxHours = sizeof(targetValuesWorldStandard) /
+                 sizeof(targetValuesWorldStandard[0]);
       MyLog::info("Auto mode started");
       xTaskCreatePinnedToCore(Fan_controller, "Fan_controller_task", 4096, NULL,
                               1, &Fan_controller_Handle, 1);
@@ -311,7 +314,7 @@ void loop() {
   }
   // Update every hour if in Auto mode
   if (isAutoMode && isRunning) {
-    if ((currentNtpTime - lastUpdateTime) >= 900) {
+    if ((currentNtpTime - lastUpdateTime) >= 3600000) {
       currentHour++;
       if (currentHour <= maxHours) {
         TARGET_PM02 = targetValues[currentHour - 1];
@@ -353,7 +356,7 @@ void Fan_controller(void *parameter) {
                      : INIT_INTERVAL_SECONDS;
 
     // Read smth
-    data0 = get_I2C_register(MAX31790, 0x22);  // Tach1 count
+    data0 = get_I2C_register(MAX31790, 0x22); // Tach1 count
     data1 = get_I2C_register(MAX31790, 0x23);
 
     uint16_t tach_out = ((((int16_t)data0) << 8) | data1) >> 5;
@@ -411,7 +414,7 @@ void Fan_controller(void *parameter) {
         pwm_bit = pwm_bit << 7;
         byte pwm0 = pwm_bit >> 8;
         byte pwm1 = pwm_bit;
-        set_I2C_register(MAX31790, 0x40, pwm0);  // Channel 1 --> Fan
+        set_I2C_register(MAX31790, 0x40, pwm0); // Channel 1 --> Fan
         set_I2C_register(MAX31790, 0x41, pwm1);
       } else {
         fanIsOn = false;
@@ -436,7 +439,7 @@ void Data_acquisition(void *parameter) {
   timeClient.update();
   unsigned long currentNtpTime = timeClient.getEpochTime();
   while (true) {
-    numSensors = 0;  // Reset sensor count for each loop
+    numSensors = 0; // Reset sensor count for each loop
     unsigned long currentNtpTime = timeClient.getEpochTime();
     for (int i = 0; i < 128; i++) {
       if (espNowMaster.getBoardId(i, boardId)) {
@@ -445,7 +448,7 @@ void Data_acquisition(void *parameter) {
           if ((currentNtpTime - timestamp) <= 5) {
             pmValues[numSensors] = pm25;
             weights[numSensors] =
-                1.0;  // Set weight to 1.0 for equal weighting, can be changed
+                1.0; // Set weight to 1.0 for equal weighting, can be changed
             numSensors++;
           }
         }
@@ -474,10 +477,10 @@ void Http_request(void *parameter) {
   while (true) {
     if (WiFi.status() == WL_CONNECTED) {
       HTTPClient https;
-      https.begin(apiUrl);  // Specify the URL
+      https.begin(apiUrl); // Specify the URL
       // Perform the GET request
       int httpCode = https.GET();
-      vTaskDelay(xDelay1000m);  // TODO: slow down the server polling
+      vTaskDelay(xDelay1000m); // TODO: slow down the server polling
       if (httpCode > 0) {
         // Check if the GET request was successful
         if (httpCode == HTTP_CODE_OK) {
@@ -488,7 +491,7 @@ void Http_request(void *parameter) {
           DeserializationError error = deserializeJson(doc, payload);
 
           if (error) {
-            https.end();  // Close connection
+            https.end(); // Close connection
             MyLog::error("%s", String("deserializeJson() failed: ").c_str());
             MyLog::error("%s", String(error.c_str()).c_str());
             vTaskDelay(xDelay1000m);
@@ -509,29 +512,29 @@ void Http_request(void *parameter) {
           //             String(meanpm02).c_str());
         }
       } else {
-        https.end();  // Close connection
+        https.end(); // Close connection
         Serial.printf("GET request failed, error: %s\n",
                       https.errorToString(httpCode).c_str());
         vTaskDelay(xDelay1000m);
         continue;
       }
-      https.end();  // Close connection
+      https.end(); // Close connection
     }
-    vTaskDelay(xDelay1000m);  // Delay before the next request
+    vTaskDelay(xDelay1000m); // Delay before the next request
   }
 }
 
 void Http_postRoom(void *parameter) {
   // Variable to track last time data was sent
   unsigned long previousMillis = 0;
-  const long interval = 60000;  // 1 minute interval
+  const long interval = 60000; // 1 minute interval
 
   while (true) {
-    unsigned long currentMillis = millis();  // Get the current time
+    unsigned long currentMillis = millis(); // Get the current time
 
     // Check if 1 minute has passed
     if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;  // Update the last send time
+      previousMillis = currentMillis; // Update the last send time
 
       if (WiFi.status() == WL_CONNECTED) {
         HTTPClient http;
@@ -540,7 +543,7 @@ void Http_postRoom(void *parameter) {
             "http://hw.airgradient.com/sensors/ffffffffffff/measures";
         http.begin(serverURL);
         http.addHeader("Content-Type",
-                       "application/json");  // Set header to JSON
+                       "application/json"); // Set header to JSON
 
         // Create JSON document
         StaticJsonDocument<200> jsonDoc;
@@ -562,7 +565,7 @@ void Http_postRoom(void *parameter) {
           // MyLog::error("Error in sending POST request");
         }
 
-        http.end();  // Close HTTP connection
+        http.end(); // Close HTTP connection
       } else {
         MyLog::debug("WiFi Disconnected");
       }
@@ -574,20 +577,20 @@ void Http_postRoom(void *parameter) {
 
 void Http_postInlet(void *parameter) {
   unsigned long previousMillis = 0;
-  const long interval = 60000;  // 1 minute interval
+  const long interval = 60000; // 1 minute interval
   while (true) {
-    unsigned long currentMillis = millis();  // Get the current time
+    unsigned long currentMillis = millis(); // Get the current time
 
     // Check if 1 minute has passed
     if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;  // Update the last send time
+      previousMillis = currentMillis; // Update the last send time
       if (WiFi.status() == WL_CONNECTED) {
         HTTPClient http;
         String serverURL =
             "http://hw.airgradient.com/sensors/94e6868d45b0/measures";
         http.begin(serverURL);
         http.addHeader("Content-Type",
-                       "application/json");  // Set header to JSON
+                       "application/json"); // Set header to JSON
         int tacho = Calculator::convertPercentageToRPM(fanSpeedPercent);
         local_pms.requestRead();
         localPmsDataValid = local_pms.readUntil(localPmsData);
@@ -611,7 +614,7 @@ void Http_postInlet(void *parameter) {
           // MyLog::error("Error in sending POST request");
         }
 
-        http.end();  // Close HTTP connection
+        http.end(); // Close HTTP connection
       } else {
         // MyLog::debug("WiFi Disconnected");
       }
